00528ef77a61ac346326d60b6d3155027ed91370
badiou <ourobadiou@gmail.com>
2022-03-15 20:23:14 +0000

    Add country select option

--- server.js
+++ server.js
@@ -2,18 +2,48 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express')
 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser=require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors=require('cors')
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
+
+//Route to get all data
+app.get('/',(req,res)=>{
+    res.send(projectData)
+});
+
+//Route for post data
+app.post('/add',(req,res)=>{
+    let newData=req.body;
+    let newEntry={
+        temp:newData.temp,
+        date:newData.date,
+        content:newData.content
+    }
+    projectData.push(newEntry);
+    res.send(projectData);
+})
+
+// create a port
+const port=8000;
 // Setup Server
+const server=app.listen(port,()=>{
+    console.log('Server running');
+    console.log(`Running on localhost: ${port}`);
+});
+
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,49 @@
 /* Global Variables */
 
+
+let baseURL='https://api.openweathermap.org/data/2.5/weather?zip=';
+let appid='&appid=aa9e528c0ae4dff30a969cc877fa2b3d';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+const btn = document.querySelector('#generate');
+btn.addEventListener('click', performAction);
+
+//here i write eprformAction function
+
+function performAction(e){
+    //console.log('hello');
+    //call getMetheo function and pass the zipcode that the user set in input element
+    //exemple zipcode=94040 exemple country=us
+  
+   const myZipCode=document.getElementById('zip').value;
+   //get the country which i have selected
+   const myCountry=document.getElementById('country').value;
+        getMetheo(baseURL,myZipCode,myCountry,appid);
+}
+
+const getMetheo=async(baseURL,zipcode,myCountry, appid)=>{
+    
+    //concat baseUrl zipcode country and  appid
+
+     let allurl=`${baseURL}${zipcode},${myCountry}${appid}`;
+    console.log(allurl);
+    const res=await fetch(allurl)
+    try
+    {
+        
+        const data=await res.json();
+        console.log(data);
+        return data;
+    }
+    catch(error)
+    {
+        console.log("error",error)
+
+    }
+    if(data.cod!='200'){
+        console.log(data[0].message);
+    }
+}
